e928e3b3f37b2cb169d5d727080ab2c6c343fdc0
peter738 <tuh10098@temple.edu>
2021-02-16 21:12:24 -0500

    hello world Flask project. Includes JS, HTML, and CSS

---
+++ static/script.js
@@ -0,0 +1,15 @@
+var ele = document.getElementById("container");
+
+ele.onclick = function () {
+    alert("HELLO WORLD");
+//    var size = 0
+//    var id = setInterval(frame, 5);
+//    function frame(){
+//        if(size == 100){
+//            clearInterval(id);
+//        } else{
+//            size++;
+//            ele.style.fontSize = size + "px";
+//        }
+//    }
+}
