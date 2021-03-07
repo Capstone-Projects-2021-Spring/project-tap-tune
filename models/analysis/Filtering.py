149c9d668703de8f79d18948863770a6834ab123
P-ramirez <ramirezpeter7@gmail.com>
2021-03-07 17:55:21 -0500

    added artist filtering, abstract class implementation

--- models/analysis/Filtering.py
+++ models/analysis/Filtering.py
@@ -5,7 +5,7 @@
 # Do I need a recording ID?
 
 import lyricsgenius
-
+from models.Database import db, get_cursor
 
 class Filtering:
     """
@@ -23,15 +23,24 @@ class Filtering:
     """
     Filter through database for records by input_artist
     Populates populates result_artist with filtered song titles
-    Return : 1 (success)
-    Return : 0 (failure)
+    Return : Data retrieved from db from filtering (success)
+    Return : None (failure)
     """
     def filterArtist(self):
 
         #checks that field is valid
         if(self.input_artist):
-
-            return 1
+            try:
+                #retrieves cursor from Database.py
+                cursor = get_cursor()
+                cursor.execute('SELECT * FROM song WHERE artist = %s', (self.input_artist, ))
+                #fetch al results and save in song_data list
+                song_data = cursor.fetchall()
+                return song_data
+                
+            except Exception as e:
+                 print(e)
+                 return None
         
         else:
 
@@ -57,8 +66,8 @@ class Filtering:
     """
     use lyricsgenius package to webscrape the Genius song collection based on input_lyrics
     Populates populates result_lyric with found song titles
-    Return : 1 (success)
-    Return : 0 (failure)
+    Return : List of artist/title pairs found from the lyric filtering (success)
+    Return : None (failure)
     """
     def filterLyrics(self):
 
@@ -69,22 +78,23 @@ class Filtering:
             client_access_token = "d7CUcPuyu-j9vUriI8yeTmp4PojoZqTp2iudYTf1jUtPHGLW352rDAKAjDmGUvEN"
 
             genius = lyricsgenius.Genius(client_access_token)
-
+            song_data = []
+            #for loop to increase number of results (current:500)
             for x in range (1,10):
+
+                #makes request to genius API and wrapper to search through lyrics 
                 request = genius.search_lyrics(self.input_lyrics, per_page=50, page=(1*x))
+                #goes through 50 songs and inserts data in song_data list
                 for hit in request['sections'][0]['hits']:
                     artist_name = hit['result']['primary_artist']['name']
                     song_title = hit['result']['title']
 
-                    print(artist_name + " - " + song_title)
-            
-            #END of cPOC
-
-            return 1
+                    song_data.append( (artist_name + "-" + song_title) )
+            return song_data
 
         else:
 
-            return 0
+            return None
     
     """
     Execution function, ordered so that most specific field is followed by least specific
@@ -94,6 +104,7 @@ class Filtering:
         filterGenre(self)
         filterLyrics(self)
 
+# do any testing in here
 if __name__ == ("__main__"):
     obj = Filtering(Lyrics = "We Will Rock You")
     success = obj.filterLyrics()
