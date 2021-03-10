3eb0c82b20fe47fca452d10e18372cb924d9d98e
Joel Jang <tug70074@temple.edu>
2021-03-09 22:54:37 -0500

    new design on front page and added link to recording rhythm page

--- static/app.js
+++ static/app.js
@@ -17,7 +17,7 @@ $('.greeting').on('click',function(e){
     alert("HELLO WORLD");
 });
 
-$('div.topnav a').on('click', function(){
-    $('a.active').removeClass('active'); // to remove the current active tab
-    $(this).addClass('active'); // add active class to the clicked tab
-});
+
+function goToRhythm() {
+      window.location.href= "/recordingRhythm"; //link from rhythm button
+}
