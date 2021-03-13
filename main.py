e51a216ba3c3b2aedb095169e188da8b788e1a5f
tuf98405 <tuf98405@temple.edu>
2021-03-13 04:33:42 -0500

    Audio Analysis and research testing. Added 1 .wav file for example.

--- main.py
+++ main.py
@@ -1,21 +1,60 @@
-from flask import Flask, render_template, request, redirect, url_for, session
-from flask_mysqldb import MySQL
-import MySQLdb.cursors
+''' Stuff for jupyter lab. not required
+%matplotlib inline
+import IPython.display as ipd
+from ipywidgets import interact
+'''
 
-app = Flask(__name__)
+import numpy as np, scipy, matplotlib.pyplot as plt
+import librosa, librosa.display
+import soundfile as sf
 
-app.config['MYSQL_HOST'] = 'taptune.cqo4soz29he6.us-east-1.rds.amazonaws.com'
-app.config['MYSQL_USER'] = 'ttapp'
-app.config['MYSQL_PASSWORD'] = '7tV9qEMc3!3Bp8M$zBSt9'
-app.config['MYSQL_DB'] = 'taptune'
+# Loads waveform of song into x
+x, sr = librosa.load('khasma.wav')
 
-mysql = MySQL(app)
+# Changes audio into harmonic/percussive wave forms
+y_harmonic, y_percussive = librosa.effects.hpss(x, margin = (1.0, 10.0))
+
+''' Not required as well to create new audio files
+# Creates and saves in a file
+sf.write('ex1.wav', y_harmonic,sr) #Melody Audio
+sf.write('ex2.wav', y_percussive, sr) #Beat Audio
+
+# Overwrite/load the percussive beat audio
+x, sr = librosa.load('ex2.wav')
+'''
+
+# Use beat track function to save the beat timestamps or frames. Tempo is the same regardless
+tempo, beat_times = librosa.beat.beat_track(y_percussive, sr=sr, units = 'time') # Time in seconds
+tempo, frames = librosa.beat.beat_track(y_percussive, sr=sr, units = 'frames') # Librosa Frames
+
+print(len(beat_times))
+print(len(frames))
+
+# Convert the beat tracked arrays
+framesToSeconds = librosa.frames_to_time(frames, sr=sr) # Frames to Seconds array
+secondsToFrames = librosa.time_to_frames(beat_times, sr=sr) # Seconds to Frames
+
+print(len(framesToSeconds))
+print(len(secondsToFrames))
+
+# Compare Converted Arrays with original
+print(framesToSeconds == beat_times)
+print(secondsToFrames == frames)
+
+# Conversion of SecondsToFrames returned some false values
+for x in range(0,len(secondsToFrames)):
+    if (secondsToFrames[x] != frames[x]):
+        print(secondsToFrames[x] - frames[x])
 
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
 
-
-if __name__ == '__main__':
-    app.run(debug=True)
+# Short Algorithm to create array of 0's and 1's on frame indicies
+array = []
+increment = 0
+for x in range(0, frames[len(frames) - 1]):
+    if (frames[increment] == x):
+        array.append(1)
+        increment += 1
+    else:
+        array.append(0)
+# print(array)
