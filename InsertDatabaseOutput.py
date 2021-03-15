0b4e22dc1310b9b4ac12ad31d2305953f154c93a
tuf98405 <tuf98405@temple.edu>
2021-03-15 02:36:05 -0400

    Adding small script to make sql inserts easier for new database info

---
+++ InsertDatabaseOutput.py
@@ -0,0 +1,14 @@
+from models.analysis import AudioAnalysis
+import librosa
+
+title = ""
+artist = ""
+release_date = ""
+genre = ""
+
+
+file = "" #path to file
+
+
+
+print("the insert statement for the thing")
