c68aa4dc6aa997ddd2ff43786274041532065360
tuf98405 <tuf98405@temple.edu>
2021-03-21 02:16:09 -0400

    Heavily Commented Code. Still WIP for certain aspects

--- ACRCloudRequest.py
+++ ACRCloudRequest.py
@@ -2,9 +2,14 @@ import os, sys
 from acrcloud.recognizer import ACRCloudRecognizer
 import re
 
+#TODO: Work on more metadata extraction, folder scan and output
 
+# Function to turn array of individual strings from split function into one string
 def arrayToString(array):
+
     string = ""
+
+    # eliminate special characters from each string and add to temp string. Skip if element is empty
     for elements in array:
         text = re.sub('[^A-Za-z0-9-_]+', '', elements)
         if elements == "":
@@ -12,12 +17,16 @@ def arrayToString(array):
         else:
             string += text + ","
 
+    # if the string ends with a comma, remove from final string
     if string[len(string) - 1] == ',':
         string = string[:len(string) - 1]
 
     return string
 
 
+
+# Song object for returning the song found from ACRCloud
+# Can Add attributes if extra metadata extraction is needed
 class foundsong:
     def __init__(self):
         self.title = ""
@@ -35,32 +44,41 @@ class foundsong:
 
 
 class acrCloudRequest:
-
     def __init__(self):
         config = {
-
+            # Should replace this with a more secure way of accessing identifiers maybe
+            'host': 'identify-eu-west-1.acrcloud.com',
+            'access_key': '5b08c65b811b20cfdfa300bca6c8a093',
+            'access_secret': '8CLfpgyw75EcTqe9UWhhs821hemenq0NiiB4bVhn',
+            'timeout': 10  # seconds
         }
         self.acr = ACRCloudRecognizer(config)
 
     def getACRSongFingerprint(self, userfile):
+        # @param userfile: audio file path
+
         # Get fingerprinted song string from ACR Cloud
         fingerprinted = self.acr.recognize_by_file(userfile, 0)
+        print(fingerprinted)
 
-        # Clean up the string for data extract
+        # Gets rid of all special characters that may not be needed. Keeps commas and hyphens
         fingerprinted = re.sub('[^A-Za-z0-9,-_]+', '', fingerprinted)
         fingerprinted = re.sub(r'[\[\]\\\/]', '', fingerprinted)
 
+        # Object for song metadata return
         songmetadata = foundsong()
         songStrings = ["title", "artists", "genres"]
 
-        # Goes through to search for each metadata
+        # Goes through to search for each metadata identifier from songStrings
         for identifiers in range(0, len(songStrings)):
             try:
                 # String manipulation for each metadata field
                 substr = fingerprinted[fingerprinted.index(songStrings[identifiers]) + len(songStrings[identifiers]) + 1:]
-                #print(substr)
+                print(substr)
 
                 # Searches for multiple entries in each field like multiple genres or artists
+                # multiple fields in identifier are separated by string "name:".
+                # Will check until next ":" is not after "name", thus ending multiple fields for said identifier
                 flag = 0
                 counter = 8
                 while flag == 0:
@@ -71,17 +89,21 @@ class acrCloudRequest:
                             flag = 1
                     counter += 1
 
-                # More String clean up
+                # More String clean up. Counter will have an index that will encompass all the fields of identifier
+                # Splits string to get names of each field
                 substr = substr[:counter]
                 substr = substr[:substr.rindex(",")]
                 substr = substr.split("name:")
 
                 # Cleans up elements in each split string and puts into a single string
                 substr = arrayToString(substr)
+
+            # If metadata identifier is not found, sets string to empty and prints error
             except ValueError:
                 print("No " + songStrings[identifiers] + " found")
                 substr = ""
 
+            # Bunch of if statements to make sure correct identifier is set to each attribute in the song object
             if songStrings[identifiers] == "title":
                 songmetadata.set_title(substr)
             if songStrings[identifiers] == "artists":
@@ -114,13 +136,11 @@ class acrCloudRequest:
 
 
 #   TESTING   ###################################################################################################################
-'''
-arr = ['', 'MasayukiSuzuki,', 'AiriSuzuki']
-print(arrayToString(arr))
 
-folderDir = "C:\\Users\\2015d\OneDrive\Desktop\.wav files"
-# file = ""C:\\Users\\2015d\OneDrive\Desktop\.wav files\DaftPuck_OneMoreTime.wav""
-file = "kaguya.wav"
+'''
+folderDir = ""
+file = ""
+#file = "kaguya.wav"
 obj = acrCloudRequest()
 obj.getACRFingerPrint_Folder(folderDir)
 
@@ -133,6 +153,6 @@ print(song.genres)
 
 
 '''
-folder = "C:\\Users\\2015d\OneDrive\Desktop\.wav files"
+folder = ""
 print(os.listdir(folder))
 '''
