30fbf895a8c18a946916b2bc6e22a7f72eacd4b2
tuf98405 <tuf98405@temple.edu>
2021-03-27 06:46:36 -0400

    Updated/Refactored FingerprintRequest.py

--- FingerprintRequest.py
+++ FingerprintRequest.py
@@ -1,4 +1,5 @@
 import os, sys
+
 from acrcloud.recognizer import ACRCloudRecognizer
 import re
 import acrcloud.acrcloud_extr_tool as ACRext
@@ -7,25 +8,17 @@ import json
 
 #TODO: Work on more metadata extraction
 
-# Function to turn array of individual strings from split function into one string
-def arrayToString(array):
+def cleanString(string):
+    # Gets rid of all special characters that may not be needed. Keeps commas and hyphens
+    newString = re.sub('[^A-Za-z0-9,-_ ]+', '', string)
+    newerString = re.sub(r'[\[\]]', '', newString)
 
-    string = ""
+    if "name: " in newString:
+        returnString = newerString.replace("name: ", '')
+    else:
+        returnString = newerString
 
-    # eliminate special characters from each string and add to temp string. Skip if element is empty
-    for elements in array:
-        text = re.sub('[^A-Za-z0-9-_]+', '', elements)
-        if elements == "":
-            continue
-        else:
-            string += text + ","
-
-    # if the string ends with a comma, remove from final string
-    if string[len(string) - 1] == ',':
-        string = string[:len(string) - 1]
-
-    return string
-
+    return returnString
 
 
 # Song object for returning the song found from ACRCloud
@@ -76,32 +69,83 @@ class FingerprintRequest:
     def getACRSongFingerprint(self, userfile):
         # @param userfile: audio file path
 
+        songArray = list()
+
         # Get fingerprinted song string from ACR Cloud
         fingerprinted = self.acr.recognize_by_file(userfile, 0)
         #print(fingerprinted)
 
-        #Service Specific naming convention
-        songStrings = ["title", "artists", "genres","score"]
+        fingerprintJson = json.loads(fingerprinted)
 
-        song = parseJSON(fingerprinted, songStrings)
-        return song
+        if 'success' not in fingerprintJson['status']:
+            print('ACRCloud: not found')
+        else:
+
+            songlist = (fingerprintJson['metadata']['music'][0])
+
+            returnsong = foundsong()
+            returnsong.set_title(cleanString(str(songlist['title'])))
+            returnsong.set_artist(cleanString(str(songlist['artists'])))
+            returnsong.set_genre(cleanString(str(songlist['genres'])))
+            returnsong.set_score(cleanString(str(songlist['score'])))
+        return returnsong
 
     def getAudDFingerprint(self, userpath):
-
         files = {
             'file': open(userpath, 'rb'),
         }
 
         # Apparently this one is use for humming and such, but has low accuracy.
+        # result = requests.post('https://api.audd.io/recognizeWithOffset/  ', data=self.data, files=files)
+        result = requests.post('https://api.audd.io/ ', data=self.data, files=files)
+
+        print(result.text)
+
+        fingerprintJson = json.loads(result.text)
+
+        if 'success' not in fingerprintJson['status']:
+            print('AudD: not found')
+        else:
+            songlist = (fingerprintJson['result'])
+
+            returnsong = foundsong()
+            returnsong.set_title(cleanString(str(songlist['title'])))
+            returnsong.set_artist(cleanString(str(songlist['apple_music']['artistName'])))
+            returnsong.set_genre(cleanString(str(songlist['apple_music']['genreNames'])))
+            # returnsong.set_score(cleanString(str(songlist['score'])))
+
+        return returnsong
+
+
+    def getHummingFingerprint(self, userpath):
+
+        files = {
+            'file': open(userpath, 'rb'),
+        }
+
+        songArray = list()
+
+        # Apparently this one is use for humming and such, but has low accuracy.
         result = requests.post('https://api.audd.io/recognizeWithOffset/  ', data=self.data, files=files)
-        # print(result.text)
         # result = requests.post('https://api.audd.io/ ', data=self.data, files=files)
 
-        # Service Specific naming convention
-        songStrings = ["title", "artists", "genreNames", "score"]
+        fingerprintJson = json.loads(result.text)
 
-        song = parseJSON(result.text, songStrings)
-        return song
+        if 'success' not in fingerprintJson['status']:
+            print('AudD Humming: not found')
+        else:
+            songlist = (fingerprintJson['result']['list'])
+            for songs in range(len(songlist)):
+                returnsong = foundsong()
+                returnsong.set_title(cleanString(str(songlist[songs]['title'])))
+                returnsong.set_artist(cleanString(str(songlist[songs]['artist'])))
+                # returnsong.set_genre(cleanString(songlist[songs]['genre']))))
+                returnsong.set_score(cleanString(str(songlist[songs]['score'])))
+
+                songArray.append(returnsong)
+
+        return songArray
+
 
     def searchFingerprintAll(self, userfile):
         audDfoundSong = self.getAudDFingerprint(userfile)
@@ -178,70 +222,6 @@ class FingerprintRequest:
 
 
 
-def parseJSON(fingerprinted, songStrings):
-    # Gets rid of all special characters that may not be needed. Keeps commas and hyphens
-    print(fingerprinted)
-    fingerprinted = re.sub('[^A-Za-z0-9,-_]+', '', fingerprinted)
-    fingerprinted = re.sub(r'[\[\]\\\/]', '', fingerprinted)
-
-    # Object for song metadata return
-    songmetadata = foundsong()
-
-    # Goes through to search for each metadata identifier from songStrings
-    for identifiers in range(0, len(songStrings)):
-        try:
-            # String manipulation for each metadata field
-            substr = fingerprinted[fingerprinted.index(songStrings[identifiers]) + len(songStrings[identifiers]) + 1:]
-            # print(substr)
-
-            # Searches for multiple entries in each field like multiple genres or artists
-            # multiple fields in identifier are separated by string "name:".
-            # Will check until next ":" is not after "name", thus ending multiple fields for said identifier
-            flag = 0
-            counter = 8
-            while flag == 0:
-                #print(substr[counter - 8:counter])
-                if len(substr) == counter:
-                    flag = 1
-                else:
-                    if substr[counter] == ':':
-                        if "name" in substr[counter - 8:counter]:
-                            counter += 1
-                        else:
-                            flag = 1
-                counter += 1
-
-            # More String clean up. Counter will have an index that will encompass all the fields of identifier
-            # Splits string to get names of each field
-            substr = substr[:counter]
-            substr = substr[:substr.rindex(",")]
-            if "name" in substr:
-                substr = substr.split("name:")
-            else:
-                substr = substr.split(",")
-
-            # Cleans up elements in each split string and puts into a single string
-            substr = arrayToString(substr)
-
-        # If metadata identifier is not found, sets string to empty and prints error
-        except ValueError:
-            #print("No " + songStrings[identifiers] + " found")
-            substr = ""
-
-        # Bunch of if statements to make sure correct identifier is set to each attribute in the song object
-        if "title" in songStrings[identifiers]:
-            songmetadata.set_title(substr)
-        if "artist" in songStrings[identifiers]:
-            songmetadata.set_artist(substr)
-        if "genre" in songStrings[identifiers]:
-            songmetadata.set_genre(substr)
-        if "score" in songStrings[identifiers]:
-            songmetadata.set_score(substr)
-
-    return songmetadata
-
-
-
 
 
 
@@ -254,12 +234,13 @@ def parseJSON(fingerprinted, songStrings):
 obj = FingerprintRequest()
 
 file = r"C:\Users\\2015d\OneDrive\Desktop\.wav files\scuffed.mp3"
-
+'''
 acrSong = obj.getACRSongFingerprint(file)
 print(acrSong.title)
 print(acrSong.artists)
 print(acrSong.genres)
 print(acrSong.score)
+'''
 
 audDSong = obj.getAudDFingerprint(file)
 print(audDSong.title)
@@ -267,12 +248,13 @@ print(audDSong.artists)
 print(audDSong.genres)
 print(audDSong.score)
 
+'''
 lastTest = obj.searchFingerprintAll(file)
 print(lastTest.title)
 print(lastTest.artists)
 print(lastTest.genres)
 print(lastTest.score)
-
+'''
 '''
 folderDir = ""
 foldersonglist = obj.getACRFingerPrint_Folder(folderDir)
