3dd04686099ae3739890bb6773ee964578d30746
XufengRen <tuk39898@temple.edu>
2021-04-24 17:51:50 -0400

    - quick fix

--- models/analysis/AudioAnalysis.py
+++ models/analysis/AudioAnalysis.py
@@ -25,33 +25,37 @@ import numpy as np
 """COMPARISON FUNCTIONS"""
 
 
-# Purpose: for pair of beats that are too closer to each other that sounds like one beat to human ears, consider them
-#          as one beat
-# Status: clear
-def drop_ambiguous(timestamp):
-    result = [timestamp[0]]
-    i = 1
-    while i <= len(timestamp) - 1:
-        if timestamp[i] - timestamp[i - 1] >= 0.08:
-            result.append(timestamp[i])
-        i += 1
-    return result
+# process the recording in full, userInput = user's timestamp sync then get pattern
+def process_recording2(userInput, songTimestamp):
+    # DB song prep
+    # songTimestamp = librosa.frames_to_time(onsetFrames, sr=22050)
+    # songTimestampSync = change_tempo(songTimestamp, 60)
+
+    # compare user input and DB info
+    # ---Decision making---
+    decision, matching_rate, header, tail = match_temposync(songTimestamp, userInput)
+    if decision == 1:
+        print("we have a match!")
+        return 1, matching_rate, header, tail
+    else:
+        return 0, matching_rate, 0, 0
 
 
-# finds difference between every 2nd timestamp
-def merge_beats(timestamps):
-    new_times = []
-    for x in range(len(timestamps) - 2):
-        dif = timestamps[x + 2] - timestamps[x]
-        new_times.append(dif)
-
-    return new_times
+# chenge timestamp to fit specific tempo k, ex change the song to 60 bpm, k=60
+def change_tempo(timestamp, k):
+    original_tempo = get_tempo(timestamp)
+    rate = original_tempo / k
+    adjusted = [i * rate for i in timestamp]
+    return adjusted
 
 
-# Purpose:  take in timestamp, return pattern
-# Principle: process timestamp and get pattern by getting time differences between each beat
-# Status: clear
-def process_timestamp_diff(timestamp):
+# get tempo from timestamp
+def get_tempo(timestamp):
+    ans = len(timestamp) * 60 / timestamp[-1]
+    return ans
+
+
+def get_pattern(timestamp):
     beat_diff = []
     for i in range(len(timestamp) - 1):
         temp = timestamp[i + 1] - timestamp[i]
@@ -59,123 +63,41 @@ def process_timestamp_diff(timestamp):
     return beat_diff
 
 
-# Purpose: take in timestamp, output pattern
-# Principle: analyze timestamp by deviding each timestamp by avg beat_diff, beat_diff: time difference between one beat
-#            to next one
-# Status: clear, waiting for tests
-def process_timestamp_ratio(timestamp):
-    beat_diff_over_avg = []
-    diff = 0
-    for i in range(len(timestamp) - 1):
-        temp = timestamp[i + 1] - timestamp[i]
-        diff += temp
-    avg_diff = diff / len(timestamp)
-    for i in range(len(timestamp)):
-        beat_diff_over_avg.append(timestamp[i] / avg_diff)
-    return beat_diff_over_avg
-
-
-# Purpose: Compare two ratio pattern and calculate matching rate
-# Principle: if user_pattern[i] / song_pattern[i] is very close t 1, it is a match beat, record that result for
-#            matching rate calculation
-# Status: In progess, error range need more testing to determine
-# Note: round both item to same decimal precision to compare?
-def compare_ratio(user_pattern, song_pattern):
-    mark = round(len(user_pattern) * 0.7)
-    numOfHit = 0
-    error = 0.2  # Working process
-    j = 0
-    match_rate = 0
-    for i in range(len(song_pattern) - len(user_pattern)):
-
-        while j < len(user_pattern):
-            match_rate_this_beat = round(1 - (abs(1 - user_pattern[j] / song_pattern[i])), 4)
-            if match_rate_this_beat >= 0.8:
-                # print("It's a hit!")
-                numOfHit += 1
-                match_rate += match_rate_this_beat
-                # print('Update match_rate: {}'.format(match_rate))
-                i += 1
-                j += 1
+def compare_sync(song_timestamp, user_pattern):
+    header = 0
+    tail = 1
+    error = 0.2
+    hit = 0
+    offset = 0
+    for i in user_pattern:
+        target = i + offset
+        while (tail < len(song_timestamp)):
+            beatlength = song_timestamp[tail] - song_timestamp[header]
+            if target - error <= beatlength <= target + error:
+                hit += 1
+                header = tail
+                tail += 1
+                offset = 0
                 break
-            elif j < len(user_pattern) - 1:
-                j += 1
+            # if we pass the target, stimulate the target
+            elif beatlength > target + error:
+                offset += i
+                break
+            # keep moving until we find a beat or miss the target
             else:
-                j = 0
-                break
-
-        # if numOfHit >= mark:
-        #     return 1
-    if numOfHit >= mark:
-        return 1, round(match_rate / numOfHit, 4)
-    else:
-        return 0, round(match_rate / len(user_pattern), 4)
+                tail += 1
+    return hit, tail
 
 
-# Purpose: Split a song into three section as a list. Iterate through the list for compare, provide early exit if song
-#          match in the first half.
-#   section one: first half
-#   Section two: Second half
-#   Section three(enhanced section): from middle of first half to middle of second half, last section to run, make sure
-#                                    no part is missed
-# Principle: compare first half first, if not match, compare with second half, if both are not match, exit. If both have
-#            some match, compare
-#            the thitd section to ensure
-# Status: Clear
-# Note: Another approach to split the song?
-def split_song(song_pattern):
-    length = len(song_pattern)
-    pattern_set = [song_pattern[0:(length / 2)], song_pattern[(length / 2):-1],
-                   song_pattern[(length / 4):(length * 3 / 4)]]
-    return pattern_set
-
-
-# function to synchronize two pattern
-def synchronize(originalPattern, base):
-    syncedPattern = []
-    for i in originalPattern:
-        syncedPattern.append(i / base)
-    return syncedPattern
-
-
-# compare input array to song
-# find out if the userPattern[a1,a2,a3...] can be found in the songPattern[s1,s2,s3...]
-# a multiple C meaning the how much faster/slower userPattern is to songPattern: a1= c*s1, a2=c*s2......
-# a pattern note is a match if the error after multiplying c is smaller than XX
-# return 1, meaning it is a match if more than XX%(70%) of the pattern note is matched
-
-# multi layer inputs: 1. compare seperately 2. combine different layers of the input and compare to the song
-# split the song acccording to the user input.
-# use hash to do comparison instead of timestamp
-def compare(userPattern, songPattern):
-    # synchronize two pattern
-    base = min(min(userPattern), min(songPattern))  # use the min in two pattern as base for synchronization
-    userSynced = synchronize(userPattern, base)
-    songSynced = synchronize(songPattern, base)
-
-    # error range
-    error = 0.5
-
-    # how many notes need to match to pass
-    mark = math.floor(len(userSynced) * 0.7)
-
-    # keep track of how many match appears
-    numOfHit = 0
-
-    for i in range(len(songSynced) - len(userSynced)):
-        for j in range(len(userSynced)):
-            if songSynced[j] - error <= userSynced[j] <= songSynced[j] + error:
-                numOfHit += 1
-        # print("# of hit : {}".format(numOfHit))
-        if numOfHit >= mark:
-            return 1
-        else:
-            numOfHit = 0
-    print("max # of hit : {}".format(numOfHit))
-    if numOfHit >= mark:
-        return 1
-    else:
-        return 0
+#
+def match_temposync(song_timestamp, user_pattern):
+    mark = 0.7 * len(user_pattern)
+    index_song_pattern = 0
+    for i in range(len(song_timestamp)):
+        hit, tail = compare_sync(song_timestamp[i:], user_pattern)
+        if hit >= mark:
+            return 1, ((hit / tail) + hit / len(user_pattern)) / 2, i, i + tail
+    return 0, 0, 0, 0
 
 
 """CONVERSION FUNCTIONS"""
@@ -400,118 +322,6 @@ def bin_to_frame(bin_array):
     return frames
 
 
-# process the recording based on peaks
-def process_recording_peaks(userInput, peakFrames):
-    # User input prep
-    new_input = merge_beats(userInput)
-    new_input_pattern = process_timestamp_ratio(new_input)
-    # DB song prep
-    timestamp = librosa.frames_to_time(peakFrames, sr=22050)
-    timestamp = drop_ambiguous(timestamp)
-    song_pattern = process_timestamp_ratio(timestamp)
-
-    # ---Decision making---
-    decision, matching_rate = compare_ratio(new_input_pattern, song_pattern)
-    if decision == 1:
-        print("we have a match!")
-        return 1, matching_rate
-    else:
-        return 0, matching_rate
-
-
-# process the recording in full
-def process_recording(userInput, onsetFrames):
-    # DB song prep
-    songTimestamp = librosa.frames_to_time(onsetFrames, sr=22050)
-    song_pattern = process_timestamp_ratio(songTimestamp)
-    # user input prep
-    input_pattern = process_timestamp_ratio(userInput)
-
-    # compare user input and DB info
-    # ---Decision making---
-    decision, matching_rate = compare_ratio(input_pattern, song_pattern)
-    if decision == 1:
-        print("we have a match!")
-        return 1, matching_rate
-    else:
-        return 0, matching_rate
-
-
-# process the recording in full, userInput = user's timestamp sync then get pattern
-def process_recording2(userInput, songTimestamp):
-    # DB song prep
-    # songTimestamp = librosa.frames_to_time(onsetFrames, sr=22050)
-    # songTimestampSync = change_tempo(songTimestamp, 60)
-
-    # compare user input and DB info
-    # ---Decision making---
-    decision, matching_rate, header, tail = match_temposync(songTimestamp, userInput)
-    if decision == 1:
-        print("we have a match!")
-        return 1, matching_rate, header, tail
-    else:
-        return 0, matching_rate, 0, 0
-
-
-# chenge timestamp to fit specific tempo k, ex change the song to 60 bpm, k=60
-def change_tempo(timestamp, k):
-    original_tempo = get_tempo(timestamp)
-    rate = original_tempo / k
-    adjusted = [i * rate for i in timestamp]
-    return adjusted
-
-
-# get tempo from timestamp
-def get_tempo(timestamp):
-    ans = len(timestamp) * 60 / timestamp[-1]
-    return ans
-
-
-def get_pattern(timestamp):
-    beat_diff = []
-    for i in range(len(timestamp) - 1):
-        temp = timestamp[i + 1] - timestamp[i]
-        beat_diff.append(temp)
-    return beat_diff
-
-
-def compare_sync(song_timestamp, user_pattern):
-    header = 0
-    tail = 1
-    error = 0.2
-    hit = 0
-    offset = 0
-    for i in user_pattern:
-        target = i + offset
-        while (tail < len(song_timestamp)):
-            beatlength = song_timestamp[tail] - song_timestamp[header]
-            if target - error <= beatlength <= target + error:
-                hit += 1
-                header = tail
-                tail += 1
-                offset = 0
-                break
-            # if we pass the target, stimulate the target
-            elif beatlength > target + error:
-                offset += i
-                break
-            # keep moving until we find a beat or miss the target
-            else:
-                tail += 1
-    return hit, tail
-
-
-#
-def match_temposync(song_timestamp, user_pattern):
-    mark = 0.7 * len(user_pattern)
-    index_song_pattern = 0
-    for i in range(len(song_timestamp)):
-        hit, tail = compare_sync(song_timestamp[i:], user_pattern)
-        if hit >= mark:
-            return 1, ((hit / tail) + hit / len(user_pattern)) / 2, i, i + tail
-    return 0, 0, 0, 0
-
-
 class rhythmAnalysis:
 
     def __init__(self, userTaps=None, filterResults=None):
@@ -685,7 +495,7 @@ class rhythmAnalysis:
             percussive_array = unhash_array(db_track.perc_hash_synced)
             percussive_frames = bin_to_frame(percussive_array)
             perc_timestamp = librosa.frames_to_time(percussive_frames, sr=22050)
-            match_percussive, matching_rate_percussive, matched_pattern = process_recording2(user_pattern_perc,
+            match_percussive, matching_rate_percussive, header,tail = process_recording2(user_pattern_perc,
                                                                                              perc_timestamp)
 
             # decide matching rate
@@ -697,7 +507,7 @@ class rhythmAnalysis:
                                                                 sr=22050)
                     song_results.append({"song": db_track,
                                          "percent_match": matching_rate_percussive,
-                                         "matched_pattern": matched_pattern,
+                                         "matched_pattern": perc_timestamp[header:tail],
                                          "sync_user_pattern": user_pattern_perc,
                                          "start_time": original_timestamp[header]})
                     max += 1
